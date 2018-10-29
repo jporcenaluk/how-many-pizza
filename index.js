26dbd2071531fbf0c959b58f580c0264efb95fd5
Studs <a.j.studnicky@gmail.com>
2018-10-29 18:15:02 -0400

    Implemented better pizza coverage

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 'use strict';
 
-var howManyPizza = function howManyPizza(people, slicesPerPerson, pizzaSlices) {
-  var slicesPerPerson = slicesPerPerson || 3;
-  var pizzaSlices = pizzaSlices || 8;
-  if ( toString.call(people) === '[object Number]' ){
-    return Math.round( people * (slicesPerPerson/pizzaSlices) );
+const isNan = (value) => {
+  //  Turns out nodejs does not have isNan() out of the box
+  return (typeof value !== 'number' || value === 0 || !value || value !== value);
+};
+
+const valOrDefault = (value, defaultVal) => {
+  return isNan(value) ?defaultVal : Math.ceil(value);
+};
+
+const howManyPizza = (people, slicesPerPerson, slicesPerPizza) => {
+
+  const pizzaToSliceRatio = valOrDefault(slicesPerPerson, 3)/valOrDefault(slicesPerPizza, 8);
+
+  if ( isNan(people) ){
+    return 0;
+  } else {
+    return Math.ceil( Math.ceil(people) * pizzaToSliceRatio );
   }
-  return 0;
+
 };
 
 module.exports = howManyPizza;
